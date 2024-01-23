4285b4001ed67c0db12dcf0b86e69c34242cee36
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-01-23 09:28:47 -0300

    Added project reference and created IEventRepository

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Persistence/IEventRepository.cs
@@ -0,0 +1,13 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Applicati.Contracts.Persistence
+{
+    public interface IEventRepository : IAsyncRepository<Event>
+    {
+    }
+}
