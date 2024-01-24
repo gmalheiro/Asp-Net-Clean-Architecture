18388ee2ef6db6eac6753b70a4600bcb864b6129
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-23 21:04:26 -0300

    Created mapping profile to map entities

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Applicati.Contracts.Features.Events;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Applicati.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap(); 
+        }
+    }
+}
