35730a605847fd35e88980dbcbdf5ea4fa524d1b
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-23 21:04:40 -0300

    Created ApplicationServiceRegistration

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/ApplicationServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Applicati
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies
+                                (AppDomain.CurrentDomain.GetAssemblies()));
+            return services;
+        }
+    }
+}
