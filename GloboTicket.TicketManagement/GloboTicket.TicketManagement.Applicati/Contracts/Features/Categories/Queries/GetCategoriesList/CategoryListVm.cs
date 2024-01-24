56e6069dd61370af60cf4e5e7c263c16da799d4a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-23 21:21:55 -0300

    Create classes to get categories list

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Categories/Queries/GetCategoriesList/CategoryListVm.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GloboTicket.TicketManagement.Applicati.Features.Categories.Queries.GetCategoriesList
+{
+    public class CategoryListVm
+    {
+        public Guid CategoryId { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace GloboTicket.TicketManagement.Applicati.Features.Categories.Queries.GetCategoriesList
+{
+    public class GetCategoriesListQuery : IRequest<List<CategoryListVm>>
+    {
+    }
+}
