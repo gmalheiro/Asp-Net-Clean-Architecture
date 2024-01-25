559aeab663681e3c8b52e71864898a544201cf51
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-25 15:45:25 -0300

    Created DeleteEvents classes

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Events/Commands/DeleteEvent/DeleteEventCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GloboTicket.TicketManagement.Applicati.Features.Categories.Commands.DeleteEvent
+{
+    public class DeleteEventCommand : IRequest
+    {
+        public Guid EventId { get; set; }
+    }
+}
---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Applicati.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Applicati.Features.Categories.Commands.DeleteEvent
+{
+    public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteEventCommandHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventToDelete = await _eventRepository.GetByIdAsync(request.EventId);
+
+            await _eventRepository.DeleteAsync(eventToDelete);
+        }
+    }
+}
