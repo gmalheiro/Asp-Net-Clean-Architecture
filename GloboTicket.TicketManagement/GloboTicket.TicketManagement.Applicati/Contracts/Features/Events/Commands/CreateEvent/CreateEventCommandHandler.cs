406063cdf1d3a1eb3659bdbed5e197cf4fd23003
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-25 15:36:26 -0300

    Created handler to create events

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Applicati.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Applicati.Contracts.Features.Events.Commands.CreateEvent
+{
+    public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Guid>
+    {
+        private readonly IEventRepository? _eventRepository;
+        private readonly IMapper? _mapper;
+
+        public CreateEventCommandHandler(IEventRepository eventRepository,IMapper mapper)
+        { 
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
+        {
+            var @event = _mapper?.Map<Event>(request);
+
+            @event = await _eventRepository!.AddAsync(@event!);
+
+            return @event.EventId;
+        }
+    }
+}
