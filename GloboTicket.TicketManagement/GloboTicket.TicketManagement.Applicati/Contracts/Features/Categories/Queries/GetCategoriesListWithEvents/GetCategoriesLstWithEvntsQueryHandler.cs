d207035405eecc81c402fe1eb7dc19af407c35ff
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-24 23:02:11 -0300

    Created GetCategoriesLstWithEvntsQueryHandler

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Categories/Queries/GetCategoriesListWithEvents/GetCategoriesLstWithEvntsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Applicati.Contracts.Persistence;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents
+{
+    public class GetCategoriesLstWithEvntsQueryHandler : IRequestHandler<GetCategoriesListWithEventsQuery, List<CategoryEventListVm>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public GetCategoriesLstWithEvntsQueryHandler(IMapper mapper, ICategoryRepository categoryRepository)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<CategoryEventListVm>> Handle(GetCategoriesListWithEventsQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _categoryRepository.GetCategoriesWithEvents(request.IncludeHistory);
+            return _mapper.Map<List<CategoryEventListVm>>(list);
+        }
+    }
+}
