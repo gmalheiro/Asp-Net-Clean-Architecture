53060cd38a3f7fe4bb5683767f766904dd6f6337
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-27 10:46:34 -0300

    Created not found exception

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Applicati.Contracts.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key ) : base ($"{name} ({key}) is not found")
+        {
+        }
+    }
+}
