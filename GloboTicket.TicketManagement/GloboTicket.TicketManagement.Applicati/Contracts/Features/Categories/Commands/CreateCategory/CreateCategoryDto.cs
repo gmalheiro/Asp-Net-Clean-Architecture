fa3a6f02a800793a9434df54a61b676739f36305
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-31 21:13:20 -0300

    Created CategoryCommand model and dto

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Categories/Commands/CreateCategory/CreateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GloboTicket.TicketManagement.Applicati.Features.Categories.Commands.CreateCateogry
+{
+    public class CreateCategoryDto
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
