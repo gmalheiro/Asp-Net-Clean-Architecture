8f337d0f559277cb664db80b739bb60608b443aa
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-02-02 18:33:47 -0300

    Created category  created response

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Features/Categories/Commands/CreateCategory/CreateCategoryCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using GloboTicket.TicketManagement.Applicati.Responses;
+
+namespace GloboTicket.TicketManagement.Applicati.Features.Categories.Commands.CreateCateogry
+{
+    public class CreateCategoryCommandResponse: BaseResponse
+    {
+        public CreateCategoryCommandResponse(): base()
+        {
+
+        }
+
+        public CreateCategoryDto Category { get; set; } = default!;
+    }
+}
