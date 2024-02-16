3b47f4678565fe9f21f61fa1a030c6960894f722
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-02-15 22:38:22 -0300

    Created IEmailService

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Applicati/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using GloboTicket.TicketManagement.Applicati.Models.Mail;
+
+namespace GloboTicket.TicketManagement.Applicati.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
