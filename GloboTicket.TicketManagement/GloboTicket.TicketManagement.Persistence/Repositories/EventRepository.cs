cde0e141d57f152d1c43e97c152af3ed76b81499
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-02-15 22:37:14 -0300

    Created EventRepository

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,23 @@
+﻿using GloboTicket.TicketManagement.Applicati.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            var matches = _dbContext.Events.Any(e => e.Name.Equals(name) && e.Date.Date.Equals(eventDate.Date));
+            return Task.FromResult(matches);
+        }
+    }
+}
