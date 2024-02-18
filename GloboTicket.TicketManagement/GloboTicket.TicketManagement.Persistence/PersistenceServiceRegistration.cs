f174479299b55326982b12181f9017da9574557f
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-02-17 21:21:06 -0300

    Created class to register persistence services

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using GloboTicket.TicketManagement.Applicati.Contracts.Persistence;
+using GloboTicket.TicketManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace GloboTicket.TicketManagement.Persistence
+{
+    public class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(IServiceCollection services
+            , IConfiguration configuration)
+        {
+            services.AddDbContext<GloboTicketDbContext>(options =>
+            options.UseSqlServer(configuration
+                                 .GetConnectionString
+                                 ("GloboTicketTicketManagementConnectionString")
+                                 ));
+            services.AddScoped<ICategoryRepository,CategoryRepository>();
+            services.AddScoped<IEventRepository,EventRepository>();
+            services.AddScoped<IOrderRepository,OrderRepository>();
+
+            return services;
+
+        }
+    }
+}
