a0d0059cac10f49377ee410cd6963abfbf169fab
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-02-17 21:17:04 -0300

    Created Service registration file to register infrastructure classes

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Infrastructu/InfrastructureServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using GloboTicket.TicketManagement.Applicati.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Applicati.Models.Mail;
+using GloboTicket.TicketManagement.Infrastructu.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Infrastructu
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
